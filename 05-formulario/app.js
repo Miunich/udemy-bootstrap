88b1cfb3fa71f075da6c0a734b13390a2c3993e1
Sergio Espinoza <sergio.es.her@gmail.com>
2023-10-03 18:08:38 -0300

    bootstrap instalacion

--- 05-formulario/app.js
+++ 05-formulario/app.js
@@ -1,6 +1,10 @@
 console.log('funcionando')
 
 const formulario = document.querySelector("#formulario")
+const btnEnviar = document.querySelector("#btnEnviar")
+const btnCargando = document.querySelector("#btnCargando")
+// guardando el toast de html
+const toast = document.querySelector(".toast")
 
 formulario.addEventListener("submit", (e)=>{
     console.log("Me diste click")
@@ -11,6 +15,19 @@ formulario.addEventListener("submit", (e)=>{
     console.log("campo Email", datos.get('emailCampo'))
     console.log("campo Password", datos.get('passwordCampo'))
     console.log("campo Checkbox", datos.get('checkCampo'))
+
+    btnEnviar.classList.add("d-none")
+    btnCargando.classList.remove("d-none")
+
+    window.setTimeout(()=>{
+
+        btnEnviar.classList.remove("d-none")
+        btnCargando.classList.add("d-none")
+
+        const eventoToast = new bootstrap.Toast(toast)
+        eventoToast.show()
+
+    },3000)
 
     formulario.reset()
 })
